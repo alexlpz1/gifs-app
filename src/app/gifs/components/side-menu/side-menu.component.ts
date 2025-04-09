6a1d159f8466c2b33495bf5b64fdd1f08759daf6
Alejandro <a.lopez@itgranada.com>
2025-04-09 09:49:31 +0200

    five commit

--- src/app/gifs/components/side-menu/side-menu.component.ts
+++ src/app/gifs/components/side-menu/side-menu.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SideMenuHeaderComponent } from "../side-menu-header/side-menu-header.component";
 import { SideMenuOptionsComponent } from "../side-menu-options/side-menu-options.component";
 
 @Component({
   selector: 'gifs-side-menu',
-  imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
   templateUrl: './side-menu.component.html',
+  imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+  isMenuOpen = false;
+  isSmallScreen = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isSmallScreen = window.innerWidth < 768; // Detecta pantallas pequeñas
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
